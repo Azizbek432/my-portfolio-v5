50ee1a71e397e2076be97b3cc27a4446602096f4
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-08-08 12:49:55 +0500

    fix: add fallback configuration in src/lib/supabase.ts for build phase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://qfutuisdbyulpiboxxan.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_3xd0EGy-pgf1CkUvlxToRA_-He2dHOC";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
